9e290550c321191d26458d257262b7129330e5dc
AndreLucenaJr <andrelucenajr123@gmail.com>
2023-05-21 20:49:36 -0300

    Menu add
    add menu

---
+++ Menu.py
@@ -0,0 +1,15 @@
+print("=" * 32)
+print("WELCOME TO THE DRAGONS AND MAGIC")
+print("=" * 32)
+
+print("=" * 32)
+print("MENU")
+print("[1] START YOUR ADVENTURE")
+print("[2] MORE ABOUT THE GAME")
+print("[3] QUIT ")
+print("=" * 32)
+
+choose = int(input("MAKE YOUR CHOISE: "))
+
+
+print("=" * 32)
